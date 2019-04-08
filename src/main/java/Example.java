1fa3345d91fc1d9040fff81a7cd1e9b6ffe7d743
sxy <789654@qq.com>
2019-04-08 10:54:36 +0800

    v0.5

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,5 +2,7 @@ public class Example {
     public static  void main(){
         System.out.println("111");
         System.out.println("222");
+        //master 分支
+        System.out.println("444 git branch sxy");
     }
 }
