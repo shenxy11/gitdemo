d9e3114383f10c8ac2eb4719228d498365460bac
sxy <789654@qq.com>
2019-04-08 11:13:59 +0800

    v0.8

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,14 +2,14 @@ public class Example {
     public static  void main(){
         System.out.println("111");
         System.out.println("222");
-<<<<<<< HEAD
+
         //master 分支
         System.out.println("444 git branch sxy");
-=======
+
         //分支333
         System.out.println("333--- git branch sxy");
         //分支444
-        System.out.println("444--- git branch sxy");
->>>>>>> sxy
+        System.out.println("444--- git branch sxy    v0.8master");
+
     }
 }
